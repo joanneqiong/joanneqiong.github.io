96fca8fa843eec4d18ba68cf75d0e4ebef0e7076
ChestFest <chestfest@protonmail.com>
2017-07-13 13:32:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,7 @@ window.onload = function () {
         if (board[0][0] == board[0][1] && board[0][0] == board[0][2] ) {
             alert("Player " + board[0][0] + " wins!");
         }
-            else if (board[1][0] == board[1][1] && board[1][0] == board[1][2] ) {
-                alert("Player " + board[1][0] + " wins!");
-            }
-             else if (board[2][0] == board[2][1] && board[2][0] == board[2][2] ) {
-                alert("Player " + board[2][0] + " wins!");
-        }
-    }
+
     
     
     function computerMoves() {
